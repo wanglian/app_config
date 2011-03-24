4af575c7b1d06ae289e90ea3414528f5ccb71a83
Wang Lian <wanglian1024@gmail.com>
2011-03-24 17:31:41 +0800

    add 'AppConfig.use_environment! Rails.env' in init.rb

--- init.rb
+++ init.rb
@@ -2,3 +2,5 @@ require 'app_config'
 
 ::AppConfig = ApplicationConfiguration.new("#{Rails.root}/config/app_config.yml",
                                            "#{Rails.root}/config/environments/#{Rails.env}.yml")
+                                           
+AppConfig.use_environment! Rails.env
